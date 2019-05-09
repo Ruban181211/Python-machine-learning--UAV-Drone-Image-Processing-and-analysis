d439fba8e1fced34b7341d1207844014182dc959
Ruban181211 <44988316+Ruban181211@users.noreply.github.com>
2019-05-10 00:00:03 +0200

    Add files via upload

---
+++ uas.py
@@ -0,0 +1,12 @@
+image = Image.open(odm_orthophoto.tif, 'r')
+    width, height = image.size
+    pixel_values = list(image.getdata())
+    if image.mode == 'RGB':
+        channels = 3
+    elif image.mode == 'L':
+        channels = 1
+    else:
+        print("Unknown mode: %s" % image.mode)
+        return None
+    pixel_values = numpy.array(pixel_values).reshape((width, height, channels))
+    return pixel_values
